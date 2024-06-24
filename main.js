73d3d8facc5384b70964d71ed723476eb45763c0
Alex23062007 <44126228+Alex23062007@users.noreply.github.com>
2024-06-24 16:20:59 +0100

    added the ability to draw

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let canvas = document.getElementById("canvas")
-let width = 64
-let height = 64
-let test = document.getElementById("testing")
+var canvas = document.getElementById("canvas");
+var width = 64;
+var height = 64;
+var test = document.getElementById("testing");
+var colour = document.getElementById("colourpicked").value;
+var body = document.getElementById("body")
+var bgColour = document.getElementById("bgcolourpicked").value;
+
+
+function updateColour() {
+    colour = document.getElementById("colourpicked").value;
+}
+
+function updateBG() {
+    bgColour = document.getElementById("bgcolourpicked").value;
+    body.setAttribute("style", "background-color:" + bgColour)
+}
 
 function createCanvas() {
     spriteValue = new Array(width * height)
@@ -9,9 +22,12 @@ function createCanvas() {
         const row = canvas.insertRow(i);
         for (j = 0; j < width; j++) {
             var cell = row.insertCell(j);
-
+            cell.addEventListener("mouseover", mouseOverCell);
+            cell.addEventListener("click", mouseClickCell)
         }
     }
+
+    console.log(colour)
 }
 
 function clearCanvas() {
@@ -19,6 +35,16 @@ function clearCanvas() {
         canvas.removeChild(canvas.firstChild);
         spriteValue = [];
     }
+}
+
+function mouseOverCell(e) {
+    if (e.buttons == 1 || e.buttons == 3) {
+        this.setAttribute("style", "background-color: " + colour)
+    }
+}
+
+function mouseClickCell(e) {
+    console.log("click!!!!")
 }
 
 test.addEventListener("click", () => {
