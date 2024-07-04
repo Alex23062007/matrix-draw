0d01f1a86aace255b4ee118ba7bcc6893c19771b
Alex23062007 <44126228+Alex23062007@users.noreply.github.com>
2024-07-05 00:01:04 +0100

    added saving (w/ base64 encoding)

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var height = 64;
 var canvasBtn = document.getElementById("makecanvas");
 var colour = document.getElementById("colourpicked").value;
 var bgColour = document.getElementById("bgcolourpicked").value;
-
-
+var savedGrid = [];
+var save = document.getElementById("save");
+var body = document.getElementById("body");
 
 function openSidebar() {
     document.getElementById("sidebar").style.left = "0";
@@ -20,7 +21,7 @@ function updateColour() {
 
 function updateBG() {
     bgColour = document.getElementById("bgcolourpicked").value;
-    document.getElementById("body").setAttribute("style", "background-color:" + bgColour)
+    body.setAttribute("style", "background-color:" + bgColour)
 }
 
 function createCanvas() {
@@ -30,12 +31,14 @@ function createCanvas() {
         for (j = 0; j < width; j++) {
             var cell = row.insertCell(j);
             cell.addEventListener("mouseover", mouseOverCell);
-            cell.addEventListener("click", mouseClickCell)
+            cell.addEventListener("click", mouseClickCell);
+            cell.setAttribute("id", "cell");
         }
     }
 
     console.log(colour)
 }
+
 
 function clearCanvas() {
     while (canvas.firstChild) {
@@ -45,7 +48,7 @@ function clearCanvas() {
 }
 
 function mouseOverCell(e) {
-    if (e.buttons == 1 || e.buttons == 3) {
+    if (e.buttons >= 1) {
         this.setAttribute("style", "background-color: " + colour)
     }
 }
@@ -54,7 +57,41 @@ function mouseClickCell(e) {
     this.setAttribute("style", "background-color: " + colour)
 }
 
+function editText(text) {
+    var newText = document.getElementById("saveTxt");
+    newText.innerHTML = btoa(`${text}`);
+    console.log(`added ${text}`);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 canvasBtn.addEventListener("click", () => {
     clearCanvas();
     createCanvas();
+});
+
+save.addEventListener("click", () => {
+    var cellList = document.querySelectorAll("#cell")
+    cellList.forEach(function (cell) {
+        if (!cell.style.backgroundColor) {
+            savedGrid.push("#000000")
+        }
+        else {
+            var str = cell.style.backgroundColor.replace(/[a-z()]/g, "");
+            rgb = str.split(", ")
+            rgb[0] = parseInt(rgb[0])
+            rgb[1] = parseInt(rgb[1])
+            rgb[2] = parseInt(rgb[2])
+            savedGrid.push(rgbToHex(rgb[0], rgb[1], rgb[2]))
+        }
+    });
+    editText(savedGrid);
+    savedGrid = [];
 });
